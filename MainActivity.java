e824d86b94dca23ed4cb874e5cc9838d0217e5fe
quankd <khucquan02012004@gmail.com>
2024-08-27 09:18:22 +0700

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.test;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.QuickContactBadge;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText A,B;
+    TextView tv;
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        A = findViewById(R.id.X);
+        B = findViewById(R.id.Y);
+        tv = findViewById(R.id.textView);
+        btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int x = Integer.parseInt(A.getText().toString());
+                int y = Integer.parseInt(B.getText().toString());
+                int result = x + y;
+                tv.setText(String.valueOf(result));
+            }
+        });
+    }
+}
